742e07f9517babbdae3c8f9625d39f213dbd7a68
ParkJiHo <iks15174@gmail.com>
2021-05-04 23:21:34 +0900

    [create]메인, 로그인, 회원가입 페이지 생성, 부트스트랩 적용 완료

--- src/main/java/com/blockgame/crash/controller/AuthController.java
+++ src/main/java/com/blockgame/crash/controller/AuthController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class CrashController {
+@RequestMapping("account")
+public class AuthController {
 
     @Autowired
     private MemberService memberService;
