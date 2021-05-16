3cf4f9715167165b433cc359c71b2ddb9d8cd598
ParkJiHo <iks15174@gmail.com>
2021-05-16 13:02:24 +0900

    [create]spring의 영속성 컨텍스트에 대해 공부하고 member와 그에 해당하는 score를 가져오는 테스트 케이스를 작성하였다.

--- src/test/java/com/blockgame/crash/CrashApplicationTests.java
+++ src/test/java/com/blockgame/crash/CrashApplicationTests.java
@@ -1,6 +1,5 @@
 package com.blockgame.crash;
 
-import com.blockgame.crash.config.auth.PrincipalDetails;
 import com.blockgame.crash.model.MemberVo;
 import com.blockgame.crash.model.RecordVo;
 import com.blockgame.crash.repository.MemberRepository;
@@ -9,8 +8,10 @@ import com.blockgame.crash.repository.RecordRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
 
 @SpringBootTest
 class CrashApplicationTests {
@@ -23,11 +24,37 @@ class CrashApplicationTests {
 
 	@Test
 	public void saveScore() {
-		Long score = (long) 10;
+		Long score = (long) 5;
 		MemberVo memberVo = memberRepository.findById("test");
 		RecordVo recordVo = new RecordVo();
 		recordVo.setScore(score);
 		recordVo.setMember(memberVo);
 		recordRepository.save(recordVo);
+	}
+
+	@Test
+	public void getRecord(){
+		List<RecordVo> recordList = recordRepository.findAll();
+		for (int i = 0; i < recordList.size(); i++){
+			System.out.println("=========================");
+			System.out.println(recordList.get(i).getRcdNo());
+			System.out.println("=========================");
+		}
+	}
+
+	@Test
+	@Transactional
+	public void getMember(){
+		List<MemberVo> memberList = memberRepository.findAll();
+		for (int i = 0; i < memberList.size(); i++){
+			System.out.println("START=========================");
+			MemberVo temp = memberList.get(i);
+			System.out.println(memberList.get(i).getId());
+			List<RecordVo> records = memberList.get(i).getRecords();
+			for(int j = 0; j < records.size(); j++){
+				System.out.println(records.get(j).getScore());
+			}
+			System.out.println("FINISH=========================");
+		}
 	}
 }
